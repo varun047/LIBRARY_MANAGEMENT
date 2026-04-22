6e185df8edb7e13da0161f8e5522f726bd9d9923
varun047 <imvbhard77@gmail.com>
2026-04-23 00:58:29 +0530

    CORS origin pushed

--- library-management-system/web_app.py
+++ library-management-system/web_app.py
@@ -23,7 +23,8 @@ if not cors_allowed_origins:
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'http://127.0.0.1:3000',
+        'https://library-management-three-inky.vercel.app'
     ]
 
 CORS(
